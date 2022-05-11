85aaa1c61e7147cd3aad68015d1f60d89a3f4aa7
Dulce G Marín <66979331+DulceGaby@users.noreply.github.com>
2022-05-10 21:33:25 -0500

    Creación del primer dto

--- gbs/Java resources/com/dispatcher/EmployeeController.java
+++ gbs/Java resources/com/dispatcher/EmployeeController.java
@@ -4,72 +4,46 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.dto.Employee;
 
 
 @Controller
 public class EmployeeController {
 	
 	@RequestMapping("/add-employee")
-	public ModelAndView addEmployee(HttpServletRequest request, HttpServletResponse responde) {
-		 ModelAndView mv = new ModelAndView();
-		 mv.setViewName("home.jsp");	
-		 return mv;
+	public String addEmployee() {
+		 return "home.jsp";
 	}
 	
 	@RequestMapping("/add-compensation")
-	public ModelAndView addCompensation(HttpServletRequest request, HttpServletResponse responde) {
-		 ModelAndView mv = new ModelAndView();
-		 mv.setViewName("addCompensation.jsp");	
-		 return mv;
+	public String addCompensation() {
+		 return "addCompensation.jsp";	
 	}
 	
 	@RequestMapping("/search")
-	public ModelAndView employees(HttpServletRequest request, HttpServletResponse responde) {
-		 ModelAndView mv = new ModelAndView();
-		 mv.setViewName("search.jsp");	
-		 return mv;
+	public String employees() {
+		 return "search.jsp";	
 	}
 	
-	@RequestMapping("/addEmployee")
-	public ModelAndView addEmployeeStore(HttpServletRequest request, HttpServletResponse responde) {
+	
+	@RequestMapping(value="/addEmployee", method=RequestMethod.POST)
+	public ModelAndView addEmployeeStore(@ModelAttribute("employee") Employee employee) {
 		System.out.println("ESTOY EN EL CONTROLADOR DE EMPLOYEE");
 		String message ="";
-		String firstName = request.getParameter("firstName");
-		String middleName = request.getParameter("middleName");
-		String lastName = request.getParameter("lastName");
-		String birthDate = request.getParameter("birthDate");
-		String position = request.getParameter("position");
+		System.out.println(employee);
+//		String firstName = request.getParameter("firstName");
+//		String middleName = request.getParameter("middleName");
+//		String lastName = request.getParameter("lastName");
+//		String birthDate = request.getParameter("birthDate");
+//		String position = request.getParameter("position");
 //		
-//		// Check if it is a correct email
-//        Pattern pattern = Pattern
-//                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-//                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-// 
-//        Matcher mather = pattern.matcher(email);
-// 
-//        if (mather.find() == false) {
-//        	message ="The email entered is invalid.";
-//            System.out.println("The email entered is invalid.");
-//        } 
-//        if(!password.equals("IBM2022")) {
-//        	message += " Incorrect password.";
-//        	System.out.println("Incorrect password.");
-//        } 
 //        
         ModelAndView mv = new ModelAndView();
-//        
-//        //If there are problems in the form, it will be redirected to the login again, with a message
-//        if(message!="") {
-//        	System.out.println("There is a message");
-//        	mv.setViewName("index.jsp");
-//        	mv.addObject("mssg",message);
-//        	mv.addObject("emailE",email);
-//        	return mv;
-//        }
-//		
-//        If everything it's ok, then redirect to the search view with the successfull mssg
         
 		mv.setViewName("search.jsp");	
 		mv.addObject("mssg","The employee was added successfully !");
